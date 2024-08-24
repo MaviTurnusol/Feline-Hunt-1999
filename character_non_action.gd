4c4ffe07d54580c3a35785fd52527baa1eb73ffc
MaviTurnusol <betatorium@gmail.com>
2024-08-24 08:41:06 +0300

    non action catelynn

---
+++ character_non_action.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if(velocity.x != 0):
+		$anima.play("walking")
+		if(velocity.x > 0):
+			$anima.flip_h = false
+		if(velocity.x < 0):
+			$anima.flip_h = true
+	else:
+		$anima.play("idle")
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
